2d93f6d29bf0ab0c4ad66c99a49d4f1ea025db4c
Roberto Nunes <jrvn@cin.ufpe.br>
2023-10-18 14:02:58 -0300

    conexao com banco de dados

--- back/src/routes/user.js
+++ back/src/routes/user.js
@@ -1,11 +1,30 @@
 const router = require('express').Router();
 
-const user = require('../models/user');
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
-    res.send({"username": "teste",
-                    "password": "123456"});
+    res.send({
+        "username": "teste",
+        "password": "123456"
+    });
 });
 
+router.post('/', async (req, res) => {
+    const {name, email, username, roles, password} = req.body;
+    const user = {
+        name,
+        email,
+        username,
+        roles,
+        password
+    };
+    try {
+        await User.create(user);
+        res.status(201).json({message: "User created successfully", user});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
 
 module.exports = router;
