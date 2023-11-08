518e1cc0baa35a288937ea2a821d941d53e1790d
Roberto Nunes <jrvn@cin.ufpe.br>
2023-11-08 16:15:21 -0300

    prontuarios 9

--- front/src/app/components/pages/PED/IS/respiratorio/respiratorio.component.ts
+++ front/src/app/components/pages/PED/IS/respiratorio/respiratorio.component.ts
@@ -6,13 +6,14 @@ import {NgForOf} from "@angular/common";
 import {MatStepperModule} from "@angular/material/stepper";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-respiratorio',
   templateUrl: './respiratorio.component.html',
   styleUrls: ['./respiratorio.component.css'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, NgForOf, MatStepperModule, FormsModule, MatButtonModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, NgForOf, MatStepperModule, FormsModule, MatButtonModule, ReactiveFormsModule, MatCheckboxModule],
 })
 export class RespiratorioComponent {
   readonly corrimento_nasal = [
@@ -21,6 +22,7 @@ export class RespiratorioComponent {
   "ausente",
   "nao_avaliado"];
   readonly opcoes = ["Sim", "Não", "Não avaliado"];
+  readonly tipo = ["produtiva", "seca", "Não avaliado"];
 
 
 }
